866b44d96926b43f3c4af95db76c19f6ec1cee08
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2022-12-05 16:10:41 +0100

    Initialisation

---
+++ Connection/connexion.php
@@ -0,0 +1,2 @@
+<?php   if (isset($_POST['nom']) && isset($_POST['pass'])) {      
+    echo "<h1>Bienvenue ".$_POST['nom'].", sur le site !</h1>";}?>
---
+++ Formulaire-et-autres/comments.php
@@ -0,0 +1,9 @@
+<?php
+    if ( isset($_POST['nom']) ) {
+        echo "<h1>Bienvenue ".$_POST['nom']." !</h1>";
+		echo "<p>Votre message : ".$_POST['msg']."</p>";
+		echo "<p>Votre langue : ".$_POST['langues']."</p>";
+		$prefs = $_POST['pref'];
+		echo "<p>Vos préférences : ".implode(" ",$prefs)."</p>";
+    }
+?>
