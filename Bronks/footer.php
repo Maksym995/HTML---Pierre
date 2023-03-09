d70c1f6fd1c0a2453121ca600367ed5f34a7faa4
Maksym Sokol <112185430+Maksym995@users.noreply.github.com>
2023-03-09 13:11:22 +0100

    mise à jour

--- Bronks/footer.php
+++
@@ -1,5 +0,0 @@
-<div class="footer">
-    <p>Rue du marché 15 - 17, 1000 Bruxelles</p>
-    <p>Horaires d’ouverture de la réception : mercredi de 13h00 à 19h30 | Téléphone: 02 219 99 21 | Email: einfo@bronks.be</p>
-    <p>@ DESIGN BY PIXEL - 2022</p>
-</div>
